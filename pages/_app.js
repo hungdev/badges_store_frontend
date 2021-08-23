12ecf7c1c3d69ab35f763dec41e381db39b8f3bd
cee <hungns126@gmail.com>
2021-08-23 23:37:19 +0700

    init add library

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
+import { wrapper } from '../store';
+import 'antd/dist/antd.css'
+import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import { Button } from 'antd';
 
 export default function Home() {
   return (
@@ -15,6 +16,8 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+        <Button type="primary">Primary Button</Button>
 
         <p className={styles.description}>
           Get started by editing{' '}
